5f5533743bbc99e8ed6c9a5186c40cc94e9f37fa
paras0602 <parasjain912@gmail.com>
2022-05-28 16:52:48 +0530

    addforkoption

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/paras0602/"
+                href="https://github.com/paras0602/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
